4ad0c47d89607e83cea85b5aec17e51aabd6937e
lisgu <87437110+SoporteUti@users.noreply.github.com>
2022-11-22 11:14:14 -0600

    empezando proyecto

---
+++ Plantilla/cabecera.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>RoaringForkCleaningService</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/roaring.png" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/letras.css" rel="stylesheet" />
+    </head>
+    <body>
